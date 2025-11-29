305c3c46f9aeb520eb36b105c976338c7d2a13bd
Arukasho <alwijeremy@gmail.com>
2025-11-29 16:38:47 +0700

    finished basic report layout

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from stats import get_character_count
+from stats import get_sorted_list
+from stats import get_word_count
 
 def main():
-    character_list = get_character_count("books/frankenstein.txt")
-    print(character_list)
+    book_directory = "books/frankenstein.txt" 
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_directory}...")
+    print("----------- Word Count ----------")
+    print(f"Found {get_word_count(book_directory)} total words")
+    print("--------- Character Count -------")
+    
+    char_list = get_sorted_list(book_directory)
+
+    for char in char_list:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+
+    print("============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -7,6 +7,9 @@ def get_word_list(file_path):
     content = get_book_text(file_path)
     return content.split()
 
+def get_word_count(file_path):
+    return len(get_word_list(file_path))
+
 def get_character_count(file_path):
     characters = {}
     word_list = get_word_list(file_path)
@@ -20,3 +23,21 @@ def get_character_count(file_path):
                 characters[char] += 1
 
     return characters
+
+def sort_num(dict):
+    return dict["num"]
+
+def get_sorted_list(file_path):
+    char_num_list = []
+    characters = get_character_count(file_path)
+
+    for char, num in characters.items():
+        char_dict = {"char": char, "num": num}
+        char_num_list.append(char_dict)
+    
+    char_num_list.sort(reverse=True, key=sort_num)
+
+    return char_num_list
+
+
+            
